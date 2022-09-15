d7b43433f1e455413f020f59ff5a2cba40339827
Kerem Apaydın <104307028+keremapaydin@users.noreply.github.com>
2022-09-15 11:41:49 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,54 @@
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import React from "react";
+import { ThemeProvider } from "styled-components/native";
+import * as firebase from "firebase";
+
+
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+
+import { theme } from "./src/infrastructure/theme";
+import { Navigation } from "./src/infrastructure/navigation";
+
+import { AuthenticationContextProvider } from "./src/services/authentication/authentication.context";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCMjZBckTe5Vb5yld3rb9KLBR-rUaRMr94",
+  authDomain: "mealstogo-48fc0.firebaseapp.com",
+  projectId: "mealstogo-48fc0",
+  storageBucket: "mealstogo-48fc0.appspot.com",
+  messagingSenderId: "666401480748",
+  appId: "1:666401480748:web:f4895493b497a3b945f189"
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export default function App() {
+  const [oswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+
+  const [latoLoaded] = useLato({
+    Lato_400Regular,
+  });
+
+  if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
+
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <AuthenticationContextProvider>
+          <Navigation />
+        </AuthenticationContextProvider>
+      </ThemeProvider>
+      <ExpoStatusBar style="auto" />
+    </>
+  );
+}
